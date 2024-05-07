c62641c6997356d60b26c26a65deccbafa046c54
Adam Stępień <vesemir2k@gmail.com>
2024-05-08 01:18:05 +0200

    📝 (App.css): add Google Fonts import for Space Mono font to improve typography in the app 📝 (tailwind.config.js): add new font family 'Space Mono' and adjust line heights for better readability and aesthetics in the app styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      space_mono: ['Space Mono'],
+    },
+    lineHeight: {
+      line_height_1: '38px',
+      line_height_2: '33px',
+      line_height_3: '25px',
+      line_height_4: '24px',
+      line_height_5: '20px',
+    },
     colors: {
-      backgroundPrimary: '#F6F8FF',
-      backgroundSecondary: '#FEFEFE',
-      textPrimary: '#222731',
-      textSecondary: '#4B6A9B',
-      backgroundPrimaryDark: '#141D2F',
-      backgroundSecondaryDark: '#1E2A47',
+      background_primary: '#F6F8FF',
+      background_secondary: '#FEFEFE',
+      text_primary: '#222731',
+      text_secondary: '#4B6A9B',
+      background_primary_dark: '#141D2F',
+      background_secondary_dark: '#1E2A47',
       white: '#FFFFFF',
       blue: '#0079FF',
       red: '#F74646',
