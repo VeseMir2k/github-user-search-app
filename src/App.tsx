e2c779df045e94fff1b93c389559fd0b895c8e6a
Adam Stępień <vesemir2k@gmail.com>
2024-05-09 21:49:23 +0200

    ✨ (App.tsx): Add Header component to the main App component to display a header section 📝 (Header.tsx, Logo.tsx, index.tsx): Add Header, Logo components and index file to organize and display the header section in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
-  return <div className='font-space_mono font-bold'>devfinder</div>;
+  return (
+    <div className='font-space_mono bg-background_primary h-[100vh] px-[24px] py-[30px]'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,3 @@
+const Logo = () => <h1 className='leading-1 text-3xl font-bold'>devfinder</h1>;
+
+export default Logo;
