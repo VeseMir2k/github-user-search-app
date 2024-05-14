fa68bb929e2bae9a762d7fea1465261e434f91b8
Adam Stępień <vesemir2k@gmail.com>
2024-05-14 21:54:32 +0200

    ✨ (Search.tsx): introduce FontAwesomeIcon component to display a magnifying glass icon for search functionality in the Search component

--- src/components/main/search/Search.tsx
+++ src/components/main/search/Search.tsx
@@ -1,7 +1,10 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ButtonSearch from './ButtonSearch';
 
 const Search = () => (
   <div className='shadow-input_shadow  w-full rounded-[15px] bg-background_secondary'>
+    <FontAwesomeIcon className='ml-[16px] size-[22px]' icon={faMagnifyingGlass} />
     <ButtonSearch />
   </div>
 );
