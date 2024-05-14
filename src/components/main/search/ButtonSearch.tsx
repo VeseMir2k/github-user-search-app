9071fc1b30aac3536cb78da42c452db360d63862
Adam Stępień <vesemir2k@gmail.com>
2024-05-14 02:05:21 +0200

    ✨ (ButtonSearch.tsx): Introduce a new button component for search functionality with specific styling ✨ (Search.tsx): Add the ButtonSearch component to the Search component to enable search functionality with a styled button

--- src/components/main/search/ButtonSearch.tsx
+++ src/components/main/search/ButtonSearch.tsx
@@ -1,3 +1,7 @@
-const ButtonSearch = () => <></>;
+const ButtonSearch = () => (
+  <button className='my-[7px] mr-[7px] rounded-[10px] bg-blue px-[16px] py-[12px] text-[14px] font-bold text-white'>
+    Search
+  </button>
+);
 
 export default ButtonSearch;
--- src/components/main/search/Search.tsx
+++ src/components/main/search/Search.tsx
@@ -1,5 +1,9 @@
+import ButtonSearch from './ButtonSearch';
+
 const Search = () => (
-  <div className='shadow-input_shadow h-[30px] w-full rounded-[15px] bg-background_secondary'></div>
+  <div className='shadow-input_shadow  w-full rounded-[15px] bg-background_secondary'>
+    <ButtonSearch />
+  </div>
 );
 
 export default Search;
