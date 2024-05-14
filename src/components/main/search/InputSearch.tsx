abe2c8a9dc70909e62649824ff93e9c2d1a3b4d6
Adam Stępień <vesemir2k@gmail.com>
2024-05-14 22:13:14 +0200

    ✨ (InputSearch.tsx): Update InputSearch component to render an input element with specific styling and placeholder text for better user experience 📝 (Search.tsx): Add InputSearch component to Search component to include a search input field with a magnifying glass icon for improved search functionality

--- src/components/main/search/InputSearch.tsx
+++ src/components/main/search/InputSearch.tsx
@@ -1,3 +1,8 @@
-const InputSearch = () => <></>;
+const InputSearch = () => (
+  <input
+    className='grow px-[7px] text-sm leading-3 text-text_secondary outline-none placeholder:text-text_secondary'
+    placeholder='Search GitHub username...'
+  ></input>
+);
 
 export default InputSearch;
--- src/components/main/search/Search.tsx
+++ src/components/main/search/Search.tsx
@@ -1,10 +1,12 @@
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ButtonSearch from './ButtonSearch';
+import InputSearch from './InputSearch';
 
 const Search = () => (
-  <div className='shadow-input_shadow  w-full rounded-[15px] bg-background_secondary'>
-    <FontAwesomeIcon className='ml-[16px] size-[22px]' icon={faMagnifyingGlass} />
+  <div className='shadow-input_shadow flex w-full items-center rounded-[15px] bg-background_secondary'>
+    <FontAwesomeIcon className='ml-[16px] size-[20px] text-blue' icon={faMagnifyingGlass} />
+    <InputSearch />
     <ButtonSearch />
   </div>
 );
