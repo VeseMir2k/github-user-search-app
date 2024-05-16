e8589b4f1701209d7df511a685fe0aa5b51d372f
Adam Stępień <vesemir2k@gmail.com>
2024-05-17 00:41:37 +0200

    📝 (User.tsx): Refactor User component to conditionally render based on userData length 📝 (UserHeader.tsx): Remove unnecessary conditional rendering for created_at date in UserHeader component

--- src/components/main/user/User.tsx
+++ src/components/main/user/User.tsx
@@ -10,12 +10,16 @@ interface Props {
 const User: React.FC<Props> = ({ userData }) => {
   const { bio } = userData;
 
+  const userDataLength = Object.keys(userData).length;
+
   return (
-    <div className='mt-[16px] rounded-[15px] bg-background_secondary px-[24px] pb-[46px] pt-[32px] shadow-shadow dark:bg-background_secondary_dark dark:[box-shadow:none]'>
-      <Header userData={userData} />
-      {bio && <Biography bio={bio} />}
-      <Statistics userData={userData} />
-    </div>
+    userDataLength && (
+      <div className='mt-[16px] rounded-[15px] bg-background_secondary px-[24px] pb-[46px] pt-[32px] shadow-shadow dark:bg-background_secondary_dark dark:[box-shadow:none]'>
+        <Header userData={userData} />
+        {bio && <Biography bio={bio} />}
+        <Statistics userData={userData} />
+      </div>
+    )
   );
 };
 
--- src/components/main/user/header/UserHeader.tsx
+++ src/components/main/user/header/UserHeader.tsx
@@ -15,11 +15,9 @@ const UserHeader: React.FC<Props> = ({ name, login, created_at }) => {
     <div className='flex flex-col'>
       <p className='text-xl font-bold dark:text-white'>{name}</p>
       <p className='text-sm text-blue'>@{login}</p>
-      {created_at && (
-        <p className='mt-[6px] text-sm text-text_secondary dark:text-white'>
-          Joined {formatDate(created_at)}
-        </p>
-      )}
+      <p className='mt-[6px] text-sm text-text_secondary dark:text-white'>
+        Joined {formatDate(created_at)}
+      </p>
     </div>
   );
 };
