871f8882e7c11367408f557e9dcbe031c850afe5
Adam Stępień <vesemir2k@gmail.com>
2024-05-17 00:27:14 +0200

    📝 (UserHeader.tsx): Update UserHeader component to support dark mode by adding dark:text-white class to elements

--- src/components/main/user/header/UserHeader.tsx
+++ src/components/main/user/header/UserHeader.tsx
@@ -13,10 +13,12 @@ const UserHeader: React.FC<Props> = ({ name, login, created_at }) => {
 
   return (
     <div className='flex flex-col'>
-      <p className='text-xl font-bold'>{name}</p>
+      <p className='text-xl font-bold dark:text-white'>{name}</p>
       <p className='text-sm text-blue'>@{login}</p>
       {created_at && (
-        <p className='mt-[6px] text-sm text-text_secondary'>Joined {formatDate(created_at)}</p>
+        <p className='mt-[6px] text-sm text-text_secondary dark:text-white'>
+          Joined {formatDate(created_at)}
+        </p>
       )}
     </div>
   );
