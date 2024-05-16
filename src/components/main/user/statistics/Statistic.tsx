7ee612b92fe828e43005609fa04edd68c7f666a2
Adam Stępień <vesemir2k@gmail.com>
2024-05-17 00:27:27 +0200

    ✨ (Statistic.tsx): Update text color classes to support dark mode for better user experience 📝 (Statistics.tsx): Add dark mode support for background color to improve user experience

--- src/components/main/user/statistics/Statistic.tsx
+++ src/components/main/user/statistics/Statistic.tsx
@@ -5,9 +5,9 @@ interface Props {
 
 const Statistic: React.FC<Props> = ({ name, statistic }) => {
   return (
-    <div className='flex flex-col gap-[8px] text-center'>
-      <p className='text-xs text-text_secondary'>{name}</p>
-      <p className='text-xl font-bold'>{statistic}</p>
+    <div className='flex flex-col gap-[8px] text-center '>
+      <p className='text-xs text-text_secondary dark:text-white'>{name}</p>
+      <p className='text-xl font-bold dark:text-white '>{statistic}</p>
     </div>
   );
 };
