ebcff984434ac7eb553867b3f89660c2fc3b7ddb
Adam Stępień <vesemir2k@gmail.com>
2024-05-16 22:25:16 +0200

    ✨ (Avatar.tsx): add styling classes to Avatar component to improve visual appearance and layout

--- src/components/main/user/header/Avatar.tsx
+++ src/components/main/user/header/Avatar.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 const Avatar: React.FC<Props> = ({ avatar_url }) => {
-  return <img src={avatar_url} alt='' />;
+  return <img className='mr-[20px] size-[70px] rounded-full' src={avatar_url} alt='avatar' />;
 };
 
 export default Avatar;
