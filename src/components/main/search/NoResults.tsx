a66d4e7071d129306e57cc590d244901542ad45d
Adam Stępień <vesemir2k@gmail.com>
2024-05-19 19:10:04 +0200

    ✨ (NoResults.tsx): Add a new component NoResults to display a message when there are no search results 📝 (Search.tsx): Include NoResults component in the Search component to show a message when there are no search results

---
+++ src/components/main/search/NoResults.tsx
@@ -0,0 +1,5 @@
+const NoResults: React.FC = () => (
+  <div className='absolute right-[100px] text-sm text-red'>No results</div>
+);
+
+export default NoResults;
--- src/components/main/search/Search.tsx
+++ src/components/main/search/Search.tsx
@@ -2,12 +2,14 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ButtonSearch from './ButtonSearch';
 import InputSearch from './InputSearch';
+import NoResults from './NoResults';
 
 const Search: React.FC = () => (
-  <div className='flex w-full items-center rounded-[15px] bg-background_secondary shadow-shadow dark:bg-background_secondary_dark dark:[box-shadow:none]'>
+  <div className='relative flex w-full items-center rounded-[15px] bg-background_secondary shadow-shadow dark:bg-background_secondary_dark dark:[box-shadow:none]'>
     <FontAwesomeIcon className='ml-[16px] size-[20px] text-blue' icon={faMagnifyingGlass} />
     <InputSearch />
     <ButtonSearch />
+    <NoResults />
   </div>
 );
 
