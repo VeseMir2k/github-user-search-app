997ca0123268dda2faf23cab18c452d29ce6e1aa
Adam Stępień <vesemir2k@gmail.com>
2024-05-20 23:51:34 +0200

    ✨ (Name.tsx): Improve layout and responsiveness by adjusting grid layout for name, login, and join date in user profile section.

--- src/components/main/user/profile/Name.tsx
+++ src/components/main/user/profile/Name.tsx
@@ -12,10 +12,12 @@ const Details: React.FC<Props> = ({ name, login, created_at }) => {
   };
 
   return (
-    <div className='flex flex-col'>
-      <p className='text-xl font-bold text-text_primary sm:text-3xl dark:text-white'>{name}</p>
-      <p className='text-sm text-blue sm:text-xl'>@{login}</p>
-      <p className='mt-[6px] text-sm text-text_secondary sm:text-base dark:text-white'>
+    <div className='flex flex-col md:grid md:w-full'>
+      <p className='text-xl font-bold text-text_primary sm:text-3xl md:col-span-1 md:col-start-1 dark:text-white'>
+        {name}
+      </p>
+      <p className='text-sm text-blue sm:text-xl md:col-span-1 md:col-start-1 '>@{login}</p>
+      <p className='mt-[6px] text-sm text-text_secondary sm:text-base md:col-span-1 md:col-start-2 md:row-start-1 md:text-right dark:text-white'>
         Joined {formatDate(created_at)}
       </p>
     </div>
